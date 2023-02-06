634d7580f12c365ade17d44c6861c612fc41798a
Sarmaon <patratiyaa@gmail.com>
2023-02-06 19:05:43 +0800

    Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите b1 ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k1 ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2 ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2 ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+var x = (-b2 + b1) / (-k1 + k2);
+var y = k2 * x + b2;
+
+Console.WriteLine($"Пересечение в точке - ({x}; {y})");
