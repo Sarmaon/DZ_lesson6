ab43902ab87f783b33c0369e2b2acb36758b0c12
Sarmaon <patratiyaa@gmail.com>
2023-02-06 18:45:09 +0800

    add task43

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+foreach (int v in array)
+{
+    if (v <= 0)
+    {
+        continue;
+    }
+    count++;
+}
+
+Console.WriteLine(value: $"Колличество введеных чисел {count}");
